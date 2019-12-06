07d8777e7019e89398ee7f6f1fca76bdb31b4a6e
soham-banerjeee <banerjeesoham04@gmail.com>
2019-12-05 21:33:55 -0500

    Fix:Updated Comments

--- db/Insert.py
+++ db/Insert.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-import Create
+import DatbaseInitialization
 engine = create_engine("sqlite:///db1.sqlite")
 session = Session(bind=engine)
-c1 = Create.Customer(first_name='Toby',
+c1 = DatbaseInitialization.Customer(first_name='Toby',
               last_name='Miller',
               username='tmiller',
               email='tmiller@example.com',
               address = '1662 Kinney Street',
               town = 'Wolfden'
               )
-c2 = Create.Customer(first_name='Scott',
+c2 = DatbaseInitialization.Customer(first_name='Scott',
               last_name='Harvey',
               username='scottharvey',
               email='scottharvey@example.com',
@@ -21,7 +21,7 @@ session.add(c1)
 session.add(c2)
 session.commit()
 
-c3 = Create.Customer(
+c3 = DatbaseInitialization.Customer(
     first_name="John",
     last_name="Lara",
     username="johnlara",
@@ -30,7 +30,7 @@ c3 = Create.Customer(
     town="Norfolk"
 )
 
-c4 = Create.Customer(
+c4 = DatbaseInitialization.Customer(
     first_name="Sarah",
     last_name="Tomlin",
     username="sarahtomlin",
@@ -39,7 +39,7 @@ c4 = Create.Customer(
     town="Norfolk"
 )
 
-c5 = Create.Customer(first_name='Toby',
+c5 = DatbaseInitialization.Customer(first_name='Toby',
               last_name='Miller',
               username='tmiller',
               email='tmiller@example.com',
@@ -47,7 +47,7 @@ c5 = Create.Customer(first_name='Toby',
               town='Wolfden'
               )
 
-c6 = Create.Customer(first_name='Scott',
+c6 = DatbaseInitialization.Customer(first_name='Scott',
               last_name='Harvey',
               username='scottharvey',
               email='scottharvey@example.com',
@@ -57,33 +57,33 @@ c6 = Create.Customer(first_name='Scott',
 
 session.add_all([c3, c4, c5, c6])
 session.commit()
-i1 = Create.Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
-i2 = Create.Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
-i3 = Create.Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
-i4 = Create.Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
-i5 = Create.Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
-i6 = Create.Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
-i7 = Create.Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
-i8 = Create.Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
+i1 = DatbaseInitialization.Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
+i2 = DatbaseInitialization.Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
+i3 = DatbaseInitialization.Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
+i4 = DatbaseInitialization.Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
+i5 = DatbaseInitialization.Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
+i6 = DatbaseInitialization.Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
+i7 = DatbaseInitialization.Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
+i8 = DatbaseInitialization.Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
 
 session.add_all([i1, i2, i3, i4, i5, i6, i7, i8])
 session.commit()
-o1 = Create.Order(customer=c1)
-o2 = Create.Order(customer=c1)
+o1 = DatbaseInitialization.Order(customer=c1)
+o2 = DatbaseInitialization.Order(customer=c1)
 
-line_item1 = Create.OrderLine(order=o1, item=i1, quantity=3)
-line_item2 = Create.OrderLine(order=o1, item=i2, quantity=2)
-line_item3 = Create.OrderLine(order=o2, item=i1, quantity=1)
-line_item3 = Create.OrderLine(order=o2, item=i2, quantity=4)
+line_item1 = DatbaseInitialization.OrderLine(order=o1, item=i1, quantity=3)
+line_item2 = DatbaseInitialization.OrderLine(order=o1, item=i2, quantity=2)
+line_item3 = DatbaseInitialization.OrderLine(order=o2, item=i1, quantity=1)
+line_item3 = DatbaseInitialization.OrderLine(order=o2, item=i2, quantity=4)
 
 session.add_all([o1, o2])
 
 session.new
 session.commit()
 
-o3 = Create.Order(customer=c1)
-orderline1 = Create.OrderLine(order=o1,item=i1, quantity=5)
-orderline2 = Create.OrderLine(order=o1,item=i2, quantity=10)
+o3 = DatbaseInitialization.Order(customer=c1)
+orderline1 = DatbaseInitialization.OrderLine(order=o1,item=i1, quantity=5)
+orderline2 = DatbaseInitialization.OrderLine(order=o1,item=i2, quantity=10)
 
 session.add_all([o3])
 
