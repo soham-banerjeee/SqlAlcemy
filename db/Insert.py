d7df9bdd2231f0fbf85033dd081542dc56386bfc
soham-banerjeee <banerjeesoham04@gmail.com>
2019-12-05 21:09:53 -0500

    Feature:Inserting  three set of values into customer data

--- db/Insert.py
+++ db/Insert.py
@@ -20,3 +20,40 @@ c2 = Create.Customer(first_name='Scott',
 session.add(c1)
 session.add(c2)
 session.commit()
+
+c3 = Create.Customer(
+    first_name="John",
+    last_name="Lara",
+    username="johnlara",
+    email="johnlara@mail.com",
+    address="3073 Derek Drive",
+    town="Norfolk"
+)
+
+c4 = Create.Customer(
+    first_name="Sarah",
+    last_name="Tomlin",
+    username="sarahtomlin",
+    email="sarahtomlin@mail.com",
+    address="3572 Poplar Avenue",
+    town="Norfolk"
+)
+
+c5 = Create.Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c6 = Create.Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+
+session.add_all([c3, c4, c5, c6])
+session.commit()
