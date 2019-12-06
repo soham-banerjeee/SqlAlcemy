19dac69fa7095fc7274a5c208e03a25ba41e05f3
soham-banerjeee <banerjeesoham04@gmail.com>
2019-12-05 20:51:40 -0500

    Feature:Added class item

--- db/DatbaseInitialization.py
+++ db/DatbaseInitialization.py
@@ -11,13 +11,22 @@ Base = declarative_base()
 class Customer(Base):
     __tablename__ = 'customers'
     id = Column(Integer(), primary_key=True)
-    first_name = Column(String(100), nullable=False)
-    last_name = Column(String(100), nullable=False)
-    username = Column(String(50), nullable=False)
-    email = Column(String(200), nullable=False)
-    address = Column(String(200), nullable=False)
+    first_name = Column(String(150), nullable=False)
+    last_name = Column(String(150), nullable=False)
+    username = Column(String(150), nullable=False)
+    email = Column(String(250), nullable=False)
+    address = Column(String(250), nullable=False)
     town = Column(String(200), nullable=False)
     created_on = Column(DateTime(), default=datetime.now)
     updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
     orders = relationship("Order", backref='customer')
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2), nullable=False)
+    quantity = Column(Integer(), nullable=False)
+    orders = relationship("OrderLine", backref='item')
 
