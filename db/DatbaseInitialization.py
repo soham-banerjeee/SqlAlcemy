f3527a1cba941711d4ccdb4e6c6f89186af6518b
soham-banerjeee <banerjeesoham04@gmail.com>
2019-12-05 20:45:20 -0500

    Feature:Added create class

--- db/DatbaseInitialization.py
+++ db/DatbaseInitialization.py
@@ -8,3 +8,16 @@ from datetime import datetime
 engine = create_engine("sqlite:///db1.sqlite")
 Base = declarative_base()
 
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(50), nullable=False)
+    email = Column(String(200), nullable=False)
+    address = Column(String(200), nullable=False)
+    town = Column(String(200), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    orders = relationship("Order", backref='customer')
+
