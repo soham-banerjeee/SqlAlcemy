c7316aa515303b931b1b97349960e78f4356a851
soham-banerjeee <banerjeesoham04@gmail.com>
2019-12-05 20:40:38 -0500

    Initial commit

---
+++ db/DatbaseInitialization.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+    Column, DateTime, ForeignKey, Numeric, SmallInteger
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
+engine = create_engine("sqlite:///db1.sqlite")
+Base = declarative_base()
+
